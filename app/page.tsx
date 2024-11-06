8b49e5fbbf145f6fcd72968b654ace4a177c29ff
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2024-11-06 09:10:52 -0400

    chore: chande remote repository

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { UserButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return <div className="h-full flex items-center justify-center">
+    <UserButton showName />
+  </div>;
+};
+
+export default Home;
