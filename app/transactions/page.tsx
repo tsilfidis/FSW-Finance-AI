4df709d76081631ec02be510896c731c3c742937
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2024-11-11 17:05:05 -0400

    feat: ensure get dashboard function is protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -15,12 +15,12 @@ const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({
     where: {
       userId,
-    }
+    },
   });
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6 overflow-hidden">
+      <div className="space-y-6 overflow-hidden p-6">
         {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
